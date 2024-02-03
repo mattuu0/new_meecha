eb0b1af1c5c7f697279fc2a1068170d15bf53bad
mattuu0 <tempacountyt@gmail.com>
2024-02-03 21:36:52 +0900

    add redis

--- meecha/location/init.go
+++ meecha/location/init.go
@@ -9,7 +9,7 @@ import (
 
 var (
 	rdb = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "redis:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 		PoolSize: 1000,
