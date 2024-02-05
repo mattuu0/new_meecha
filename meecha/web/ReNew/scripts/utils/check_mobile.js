96dcbd69cb2e4c97d30a635e6a54bbdadef2abed
mattuu0 <tempacountyt@gmail.com>
2024-02-05 18:33:29 +0900

    remove keys

--- meecha/web/ReNew/scripts/utils/check_mobile.js
+++
@@ -1,6 +0,0 @@
-var ua = navigator.userAgent;
-if (!(ua.indexOf('iPhone') > 0 || ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0 || ua.indexOf('Mobile') > 0 )) {
-    console.log("モバイル");
-    window.location.href = desktop_url;
-}
-
--- meecha/web/ReNew/scripts/utils/desktop_show.js
+++
@@ -1,3 +0,0 @@
-var qrcode = new QRCode("qrcode");
-
-qrcode.makeCode(server_url);
